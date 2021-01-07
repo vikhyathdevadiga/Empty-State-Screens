aad07c39203fa499e61287d06d0549c77e84f7d3
Ashish Madaan <vikhyaths1@users.noreply.github.com>
2021-01-08 00:09:27 +0530

    Initial commit

---
+++ app/src/main/java/com/vs/emptystates/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vs.emptystates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.error_404);
+    }
+}
